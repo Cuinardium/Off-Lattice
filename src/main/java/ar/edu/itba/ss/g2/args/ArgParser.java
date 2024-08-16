a875d5e7e0615060c8cd690c1d58c906bd21947a
Santiago Ballerini <sballerini@itba.edu.ar>
2024-08-16 15:13:53 -0300

    feat: add argument parsing

---
+++ src/main/java/ar/edu/itba/ss/g2/args/ArgParser.java
@@ -0,0 +1,170 @@
+package ar.edu.itba.ss.g2.args;
+
+import org.apache.commons.cli.*;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class ArgParser {
+
+    private static final List<Option> OPTIONS =
+            List.of(
+                    new Option("h", "help", false, "Show help"),
+                    new Option("out", "output", true, "Output directory"),
+
+                    // For particle generation
+                    new Option("s", "seed", true, "Seed for random number generator"),
+                    new Option("N", "number", true, "Amount of particles to be generated"),
+                    new Option("L", "length", true, "Length of the area to be analyzed"),
+
+                    // For Cell Index Method
+                    new Option("v", "velocity", true, "Velocity of the particles"),
+                    new Option("n", "noise", true, "Noise in the velocity of the particles"),
+                    new Option("rc", "interaction-radius", true, "Interaction radius"));
+
+    private final String[] args;
+    private final Options options;
+
+    public ArgParser(String[] args) {
+        this.args = args;
+
+        Options options = new Options();
+        OPTIONS.forEach(options::addOption);
+        this.options = options;
+    }
+
+    public Configuration parse() {
+
+        CommandLineParser parser = new DefaultParser();
+
+        CommandLine cmd;
+
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.err.println("Error parsing arguments: " + e.getMessage());
+            return null;
+        }
+
+        if (cmd.hasOption("h")) {
+            return null;
+        }
+
+        Configuration.Builder builder = new Configuration.Builder();
+
+        // Particle generation parameters
+
+        if (cmd.hasOption("N")) {
+            int N;
+            try {
+                N = Integer.parseInt(cmd.getOptionValue("N"));
+            } catch (NumberFormatException e) {
+                System.err.println("Invalid number of particles: " + cmd.getOptionValue("N"));
+                return null;
+            }
+
+            builder.N(N);
+        } else {
+            System.err.println("Number of particles is required");
+            return null;
+        }
+
+        if (cmd.hasOption("L")) {
+            int L;
+            try {
+                L = Integer.parseInt(cmd.getOptionValue("L"));
+            } catch (NumberFormatException e) {
+                System.err.println("Invalid length: " + cmd.getOptionValue("L"));
+                return null;
+            }
+
+            builder.L(L);
+
+        } else {
+            System.err.println("Length is required");
+            return null;
+        }
+
+        if (cmd.hasOption("s")) {
+            int seed;
+            try {
+                seed = Integer.parseInt(cmd.getOptionValue("s"));
+            } catch (NumberFormatException e) {
+                System.err.println("Invalid seed: " + cmd.getOptionValue("s"));
+                return null;
+            }
+
+            builder.seed(seed);
+        }
+
+        // Simulation parameters
+
+        if (cmd.hasOption("v")) {
+            double v;
+            try {
+                v = Double.parseDouble(cmd.getOptionValue("v"));
+            } catch (NumberFormatException e) {
+                System.err.println("Invalid velocity: " + cmd.getOptionValue("v"));
+                return null;
+            }
+
+            builder.v(v);
+        } else {
+            System.err.println("Velocity is required");
+            return null;
+        }
+
+        if (cmd.hasOption("n")) {
+            double noise;
+            try {
+                noise = Double.parseDouble(cmd.getOptionValue("n"));
+            } catch (NumberFormatException e) {
+                System.err.println("Invalid noise: " + cmd.getOptionValue("n"));
+                return null;
+            }
+
+            builder.noise(noise);
+        } else {
+            System.err.println("Noise is required");
+            return null;
+        }
+
+        if (cmd.hasOption("rc")) {
+            double rc;
+            try {
+                rc = Double.parseDouble(cmd.getOptionValue("rc"));
+            } catch (NumberFormatException e) {
+                System.err.println("Invalid cutoff radius: " + cmd.getOptionValue("rc"));
+                return null;
+            }
+
+            builder.rc(rc);
+        } else {
+            System.err.println("Cutoff radius is required");
+            return null;
+        }
+
+        if (cmd.hasOption("out")) {
+            builder.outputDirectory(cmd.getOptionValue("out"));
+        } else {
+            System.err.println("Output directory is required");
+            return null;
+        }
+
+        return builder.build();
+    }
+
+    public void printHelp() {
+
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.setOptionComparator(Comparator.comparingInt(OPTIONS::indexOf));
+
+        formatter.setLeftPadding(4);
+        formatter.setWidth(120);
+
+        String commandLineSyntax =
+                "java -jar off-lattice-1.0-SNAPSHOT-jar-with-dependencies.jar [options]";
+
+        formatter.printHelp(commandLineSyntax, options);
+    }
+}
---
+++ src/main/java/ar/edu/itba/ss/g2/args/Configuration.java
@@ -0,0 +1,125 @@
+package ar.edu.itba.ss.g2.args;
+
+import java.util.Random;
+
+public class Configuration {
+
+    private final String outputDirectory;
+
+    private final int seed;
+
+    private final int N;
+    private final int L;
+
+    private final double v;
+    private final double noise;
+    private final double rc;
+
+    private Configuration(Builder builder) {
+        this.outputDirectory = builder.outputDirectory;
+        this.seed = builder.seed;
+        this.N = builder.N;
+
+        this.v = builder.v;
+        this.noise = builder.noise;
+        this.L = builder.L;
+        this.rc = builder.rc;
+    }
+
+    public String getOutputDirectory() {
+        return outputDirectory;
+    }
+
+    public int getSeed() {
+        return seed;
+    }
+
+    public int getN() {
+        return N;
+    }
+
+    public int getL() {
+        return L;
+    }
+
+    public double getV() {
+        return v;
+    }
+
+    public double getNoise() {
+        return noise;
+    }
+
+    public double getRc() {
+        return rc;
+    }
+
+    @Override
+    public String toString() {
+        return "Configuration{" +
+                "outputDirectory='" + outputDirectory + '\'' +
+                ", seed=" + seed +
+                ", N=" + N +
+                ", L=" + L +
+                ", v=" + v +
+                ", noise=" + noise +
+                ", rc=" + rc +
+                '}';
+    }
+
+    public static class Builder {
+
+        private String outputDirectory;
+
+        private int seed = new Random().nextInt();
+
+        private int N;
+        private int L;
+
+        private double v;
+        private double noise;
+
+        private double rc;
+
+        public Builder() {}
+
+        public Builder seed(int seed) {
+            this.seed = seed;
+            return this;
+        }
+
+        public Builder outputDirectory(String outputDirectory) {
+            this.outputDirectory = outputDirectory;
+            return this;
+        }
+
+        public Builder N(int N) {
+            this.N = N;
+            return this;
+        }
+
+        public Builder L(int L) {
+            this.L = L;
+            return this;
+        }
+
+        public Builder v(double v) {
+            this.v = v;
+            return this;
+        }
+
+        public Builder noise(double noise) {
+            this.noise = noise;
+            return this;
+        }
+
+        public Builder rc(double rc) {
+            this.rc = rc;
+            return this;
+        }
+
+        public Configuration build() {
+            return new Configuration(this);
+        }
+    }
+}
