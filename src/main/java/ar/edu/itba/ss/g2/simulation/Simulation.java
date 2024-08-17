c6f7fc835241fdc1703afe5db7d21398641cb9e8
Santiago Ballerini <sballerini@itba.edu.ar>
2024-08-17 12:11:02 -0300

    feat: implement simulation

---
+++ src/main/java/ar/edu/itba/ss/g2/simulation/Simulation.java
@@ -0,0 +1,41 @@
+package ar.edu.itba.ss.g2.simulation;
+
+import ar.edu.itba.ss.g2.Particle;
+import ar.edu.itba.ss.g2.BoardState;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class Simulation {
+
+    private final List<Set<Particle>> snapshots;
+
+    private final BoardState boardState;
+
+    private final long t;
+
+    public Simulation(Set<Particle> particles, int L, double v, double noise, double rc, long t) {
+
+        this.boardState = new BoardState(L, v, noise, rc, particles);
+
+        this.t = t;
+
+        this.snapshots = new ArrayList<>((int) t);
+    }
+
+    public void run() {
+
+        snapshots.add(boardState.getParticles());
+
+        for (int i = 1; i < t; i++) {
+            boardState.updateBoardState();
+            snapshots.add(boardState.getParticles());
+        }
+    }
+
+    public List<Set<Particle>> getSnapshots() {
+        return snapshots;
+    }
+
+}
