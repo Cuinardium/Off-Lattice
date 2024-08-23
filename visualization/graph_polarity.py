5f63d41ed53c88ac26931f681db7b9eeef8d7ba8
Santiago Ballerini <sballerini@itba.edu.ar>
2024-08-23 12:02:51 -0300

    feat: plot multiple polarities

--- visualization/graph_polarity.py
+++ visualization/graph_polarity.py
@@ -27,7 +27,29 @@ def plot_polarity(time_steps, angles, vel, output_file='data/polarity.png'):
 
     plt.savefig(output_file)
     
+def plot_multiple_polarities(time_steps, multiple_angles, vel, output_file='data/polarities.png'):
+    _, ax = plt.subplots()
 
+    for angles in multiple_angles:
+        polarities = []
+        for i in range(len(time_steps)):
+            # vx: cos(angle), vy: sin(angle)
+            vx, vy = np.cos(angles[i]) * vel, np.sin(angles[i]) * vel
+
+            # Add all vectors and calculate magnitude
+            v_sum = np.sum(vx), np.sum(vy)
+            mag = np.linalg.norm(v_sum)
+            polarity = mag / (len(angles[i]) * vel)
+            polarities.append(polarity)
+
+        ax.plot(time_steps, polarities)
+
+    ax.set_xlim(0, time_steps[-1])
+    ax.set_ylim(0, 1)
+    ax.set_xlabel('Time step')
+    ax.set_ylabel('Polarity')
+
+    plt.savefig(output_file)
 
 
 
