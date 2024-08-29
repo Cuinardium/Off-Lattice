65ae5885d05e7b6edf35a4c162f64e58a195df17
Franco Bosetti <francobosetti@live.com.ar>
2024-08-28 21:06:40 -0300

    feat: plot means and deviations

--- visualization/graph_polarity.py
+++ visualization/graph_polarity.py
@@ -63,6 +63,31 @@ def plot_multiple_polarities(time_steps, multiple_angles, labels, vel, output_fi
     plt.savefig(output_file)
     plt.close(fig)
 
+def plot_means_and_deviations(means, deviations, labels, output_file='data/means_deviations.png'):
+    fig, ax = plt.subplots()
+
+    # Ensure the number of labels matches the number of angle sets
+    if len(labels) != len(means):
+        raise ValueError("Number of labels must match the number of angle sets")
+
+    for mean, deviation, label in zip(means, deviations, labels):
+        ax.errorbar(mean, deviation, label=label)
+
+    ax.set_xlim(0, 1)
+    ax.set_ylim(0, 1)
+    ax.set_xlabel('Mean')
+    ax.set_ylabel('Deviation')
+
+    # Shrink current axis by 20%
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+
+    # Put a legend to the right of the current axis
+    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+
+    plt.savefig(output_file)
+    plt.close(fig)
+
 
 
 
