44435b9538c40395a18845d8049a12d4b7fcb7e9
Santiago Ballerini <sballerini@itba.edu.ar>
2024-08-29 16:27:23 -0300

    feat: add ca;culate polarities

--- visualization/utils.py
+++ visualization/utils.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def parse_dynamic_file(file_path):
     time_steps = []
     positions = []
@@ -31,3 +33,21 @@ def parse_static_file(file_path):
     noise = float(lines[3].strip())
     rc = float(lines[4].strip())
     return L, seed, v, noise, rc
+
+
+
+def calculate_polarities(angles, vel):
+
+    polarities = []
+    for angle_list in angles:
+        # vx: cos(angle), vy: sin(angle)
+        vx, vy = np.cos(angle_list) * vel, np.sin(angle_list) * vel
+
+        # Add all vectors and calculate magnitude
+        v_sum = np.sum(vx), np.sum(vy)
+        mag = np.linalg.norm(v_sum)
+        polarity = mag / (len(angle_list) * vel)
+        polarities.append(polarity)
+
+
+    return polarities
